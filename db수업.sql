59a29100ffecce528af72024a74c340abb475dd7
haenocklee <haenock7706@naver.com>
2024-01-03 15:54:42 +0900

    dbClass20240103

--- db수업.sql
+++ db수업.sql
@@ -213,6 +213,105 @@ select count(*) from emp;
 select count(*) from emp where deptno = 20;
 
 
+-- 그룹화(gruping, 특정 컬럼을 기준으로 동일한 데이터 끼리 묶는 것)
+-- 그룹화한 결과를 조회할 때는 그룹화한 컬럼으로 조회만 가능
+-- job을 기준으로
+-- 그룹화하지 않은 컬럼은 조회 불가능 
+select * from emp group by job; -- x
+select job, ename from emp group by job; -- x
 
+select job from emp group by job;
+-- 직급별로 편균급여
+select job , avg(sal) from emp group by job;
+-- 직급이 clerk 인 사원들의 평균급여 
+select job, avg (sal) from emp where job = 'clerk';
+-- 부서별(depno)별 평균 급여
+select depno, avg(sal) from emp group by deptno;
+-- 직급별 사원수 
+select job , count(*) from emp group by job;
+-- 직급별 평균급여, 사원수
+select job, avg(sal), count(*) from emp group by job;
+-- 위 결과에서 직급기준 오름차순 정렬
+select job, avg(sal), count(*) from emp group by job order by job asc; 
+-- 위의 결과에서 평균 급여 소수 첫째자리 까지 
+select job, round(avg(sal),1), count(*) from emp group by job order by job asc; 
+-- 위의 결과에서 job은 '직급' 급여 평균값은 '평균급여' 사원수는 '사원수' 로 표편하여 조회
+select job as '직급', round(avg(sal),1) as '평균급여', count(*) as '사원수' from emp group by job order by job asc; 
 
+-- 두 개 이상의 컬럼 그룹화 -- 그룹화 한 결과에는 where 사용 불가
+-- deptno 를 기준으로 그룹화하고 그 결과에서 job을 기준으로 그룹화
+select deptno, job from emp group by deptno, job;
+select deptno, job, count(*) from emp group by deptno, job;
+select deptno, job, count(*), avg(sal) from emp group by deptno, job;
 
+-- 그룹화한 결과에 조건을 적용한다면 
+-- having 
+-- 위의 결과에서 평균 급여가 2000 이상인 결과만 조회
+select deptno, job, count(*), avg(sal)
+	from emp
+		group by deptno, job
+			having avg(sal) >= 2000;
+
+select deptno, job, count(*), avg(sal)
+from emp
+	group by deptno, job
+		having avg(sal) >= 2000
+			order by deptno asc;
+
+-- 급여가 3000 이하인 사원만 가지고 부서별, 직급별 그룹화를 하고 평균급여가 2000 이상인 결과 조회 
+select deptno, job, count(*), avg(sal)
+	from emp
+		where sal <= 3000
+			group by deptno, job
+				having avg(sal) >= 2000
+					order by deptno asc;
+                    
+/*
+	연습문제
+	1. 부서별 평균급여, 최고급여, 최저급여, 사원수 조회(평균급여는 소수점 둘째자리에서 반올림)
+    2. 직급별 사원수 조회(단 3명 이상인 결과만 출력)
+    3. 연도별 입사한 사원수 조회(조회결과 : 연도(yyyy), 사원수)
+    3.1. 위의 결과에서 각 연도별로 부서별 입사한 사원수 조회(조회결과 : 연도(yyyy), 부서번호, 사원수)
+*/
+select  deptno as '부서번호', round(avg(sal),2)as'평균급여', max(sal)as'최고급여', min(sal)as'최저급여', count(*) as'사원수' 
+	from emp 
+		group by deptno -- 1
+			order by deptno asc;
+            
+select job as '직급', count(*) as '사원수'  -- 2
+	from emp 
+		group by job having count(*) >= 3;
+    
+select date_format(hiredate, '%Y')as'연도',count(*)as'사원수'  -- 3
+	from emp 
+		group by date_format(hiredate, '%Y');
+    
+select date_format(hiredate, '%Y')as'연도',deptno as '',count(*)as'사원수'  -- 3.1
+	from emp 
+		group by date_format(hiredate, '%Y'),deptno
+			order by date_format(hiredate, '%Y') asc;
+
+-- 조인(join)
+-- 두 개 이상의 테이블ㅇ르 하나의 테이블처럼 사용할 때 
+select * from emp;
+select * from dept;
+select * from emp, dept;-- 적절치 못한 조인방법
+-- 내부 조인(inner join)
+-- 조인하려는 두테이브에 공통 컬럼이 존재해야 함 
+select*from emp, dept where emp.deptno = dept.deptno;
+-- 테이블 이름을 줄여서 사용
+select*from emp e, dept d where e.deptno = d.deptno;
+
+-- deptno 컬럼은 두테이블에 모두 있기 때문에 ambigiuous 에러
+select empno, ename, job, deptno, dname, loc 
+	from emp e, dept d where e.deptno = d.deptno;
+
+select e.empno, e.ename, e.job, e.deptno, d.dname, d.loc 
+	from emp e, dept d where e.deptno = d.deptno;
+-- emp 테이블 전체 조회
+select e.* from emp e, dept d where e.deptno = d.deptno;
+-- emp, dept를 조인하여 empno,ename, deptno, dname, loc 조회
+-- (단, 급여가 2500 이상인 사원만 조회하고, 조회 결과는 사원이름 기준으로 오름차순 정렬)
+select e.empno, e.ename, e.job, e.deptno, d.dname, d.loc 
+	from emp e, dept d where e.deptno = d.deptno and e.sal >= 2500
+		order by e.ename asc;
